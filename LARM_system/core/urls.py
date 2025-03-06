531dc1745b5d18610dde3ce475ef26cba450c059
Cyitron <gclencina@hotmail.com>
2025-03-05 23:55:14 -0300

    version 0,2

--- LARM_system/core/urls.py
+++ LARM_system/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import login_view, cadastro_view
+from .views import login_view, cadastro_view, main_menu, inventario, admin_cadastro
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('cadastro/', cadastro_view, name='cadastro'),
+    path('menu/', main_menu, name="main_menu"),
+    path('inventario/', inventario, name="inventario"),
+    path('admin-cadastro/', admin_cadastro, name="admin-cadastro")
 ]
--- LARM_system/core/views.py
+++ LARM_system/core/views.py
@@ -10,4 +10,13 @@ def cadastro_view(request):
     return render(request, 'cadastro.html')
 
 def main_menu(request):
-    return render(request, 'main_menu.html')
+    # Verifica se o usuário está autenticado e se é administrador.
+    # Você pode usar is_staff ou is_superuser conforme sua lógica.
+    is_admin = request.user.is_authenticated and request.user.is_staff  
+    return render(request, 'main_menu.html', {'is_admin': is_admin})
+
+def inventario(request):
+    return render(request, 'inventario.html')
+
+def admin_cadastro(request):
+    return render(request, "admin-cadastro.html")
